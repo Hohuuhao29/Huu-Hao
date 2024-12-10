f0b8c505f89f04da271b97a6c8708359faa834b9
Hohuuhao29 <huuhao15209@gmail.com>
2024-12-10 10:46:13 +0700

    Add files via upload

---
+++ typeData/src/basic/dev/MainApp.java
@@ -0,0 +1,31 @@
+package basic.dev;
+
+import java.util.Scanner;
+
+public class MainApp {
+    public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    	// Thong tin mot sinh vien
+		String ten = "Huu Hao";
+		String msv = "2557554";
+		String cmnd = "0561548138";
+		String tl = "10A12";
+		String tt = "Cao Dang Cong Nghiep Hue";
+		double dtb = 9.8;
+		
+		// Thong tin cong nhan 
+		String t = "Ho Huu Hao";
+		String dc = "70 Nguyen Hue";
+		String sdt = "0317924674";
+		double hsl = 9.000;
+		 
+		// Thong tin mot chiec xe car
+		String te = "Hao";
+		String h = "Maybach";
+		double g = 3.000;
+		String ms = "Pink";
+		
+		
+	}
+
+}
